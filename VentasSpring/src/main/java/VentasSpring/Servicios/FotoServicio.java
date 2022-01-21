f600bc20b53d3b719b6bcf775931304734207363
Ignacio Barolo <baroloignacio@gmail.com>
2022-01-20 21:40:08 -0300

    Agregando validaciones y carga de imagenes a registro-producto

--- VentasSpring/src/main/java/VentasSpring/Servicios/FotoServicio.java
+++ VentasSpring/src/main/java/VentasSpring/Servicios/FotoServicio.java
@@ -3,6 +3,8 @@ package VentasSpring.Servicios;
 import VentasSpring.Entidades.Foto;
 import VentasSpring.Repositorios.FotoRepositorio;
 import java.util.Optional;
+import javax.imageio.ImageIO;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,8 +19,14 @@ public class FotoServicio {
     @Autowired
     private FotoRepositorio fotoRepositorio;
 
+    @Transactional
     public Foto guardarFoto(MultipartFile archivo) throws Exception {
-        if (!archivo.isEmpty()) {
+
+        if (archivo.getContentType().equals("application/octet-stream") || archivo.isEmpty()) {
+            return null;
+        }
+
+        if (validarExtension(archivo)) {
 
             try {
                 Foto foto = new Foto();
@@ -30,12 +38,38 @@ public class FotoServicio {
             } catch (Exception e) {
                 throw new Exception(e.getMessage());
             }
+
         } else {
-            return null;
+            throw new Exception("El archivo debe ser de tipo imagen.");
         }
 
     }
 
+    /*Valida: si el archivo es una imagen devuelve un Boolean.TRUE, caso contrario significa que 
+    no es una imagen y devuelve un Boolean.FALSE
+     */
+    public Boolean validarExtension(MultipartFile archivo) throws Exception {
+        try {
+            ImageIO.read(archivo.getInputStream()).toString();
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            return Boolean.FALSE;
+        }
+    }
+
+    public Boolean validarArchivoVacio(MultipartFile archivo) throws Exception {
+        try {
+            if (archivo.getContentType().equals("application/octet-stream")) {
+                return null;
+            }
+            ImageIO.read(archivo.getInputStream()).toString();
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            return Boolean.FALSE;
+        }
+    }
+
+    @Transactional
     public Foto modificarFoto(String id, MultipartFile archivo) throws Exception {
         if (archivo != null) {
 
--- VentasSpring/src/main/java/VentasSpring/Servicios/ProductoServicio.java
+++ VentasSpring/src/main/java/VentasSpring/Servicios/ProductoServicio.java
@@ -1,12 +1,16 @@
 package VentasSpring.Servicios;
 
+import VentasSpring.Entidades.Foto;
 import VentasSpring.Entidades.Producto;
 import VentasSpring.Errores.ErrorServicio;
 import VentasSpring.Repositorios.ProductoRepositorio;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -14,70 +18,103 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ProductoServicio {
-    
+
     @Autowired
     private ProductoRepositorio productoRepositorio;
-    
+
+    @Autowired
+    private FotoServicio fotoServicio;
+
     @Transactional
-    public void registrarProducto(String nombre, Double precio, String descripcion, 
-            String fabricante, Integer stock){
-        //Validar datos
+    public void registrarProducto(String nombre, Double precio, String descripcion,
+            String fabricante, Integer stock, List<MultipartFile> archivos) throws Exception {
+
+        if (productoRepositorio.buscarPorNombre(nombre) != null) {
+            throw new Exception("Ya existe un producto con ese nombre.");
+        }
+
         Producto producto = new Producto();
-        
+
         producto.setNombre(nombre);
         producto.setPrecio(precio);
         producto.setDescripcion(descripcion);
         producto.setFabricante(fabricante);
         producto.setStock(stock);
         producto.setAlta(Boolean.TRUE);
-        
+
+        /*Recibo la lista de archivos(fotos), creo una lista local, recorro la lista que recibo guardo cada foto y
+        la añado en la lista local. Como último paso le seteo a "producto" la lista local con todas las fotos
+        guardadas. En caso de estar vacía la lista, se lanza una Exception requeriendo cargar al menos 1 foto
+         */
+        List<Foto> listaFotos = new ArrayList();
+        for (MultipartFile auxArchivo : archivos) {
+            listaFotos.add(fotoServicio.guardarFoto(auxArchivo));
+        }
+
+        if (!validarImagenNula(listaFotos)) {
+            throw new Exception("Por favor, cargue al menos 1 imagen del producto.");
+        }
+
+        producto.setFotos(listaFotos);
+
         productoRepositorio.save(producto);
     }
-    
+
+    //Valida que no se cargue la lista nula, ya que es necesario que el producto se cargue con imagenes
+    public Boolean validarImagenNula(List<Foto> listaFotos) throws Exception {
+
+        for (Foto auxArchivos : listaFotos) {
+            if (auxArchivos == null) {
+                return Boolean.FALSE;
+            }
+        }
+        return Boolean.TRUE;
+    }
+
     @Transactional
-    public void modificarProducto(String id, String nombre, Double precio, String descripcion, 
-            String fabricante, Integer stock) throws ErrorServicio{
+    public void modificarProducto(String id, String nombre, Double precio, String descripcion,
+            String fabricante, Integer stock) throws ErrorServicio {
         //Validar datos
         Optional<Producto> respuestaProducto = productoRepositorio.findById(id);
-        if(respuestaProducto.isPresent()){
+        if (respuestaProducto.isPresent()) {
             Producto producto = respuestaProducto.get();
-            
+
             producto.setNombre(nombre);
             producto.setPrecio(precio);
             producto.setDescripcion(descripcion);
             producto.setFabricante(fabricante);
             producto.setStock(stock);
-            
+
             productoRepositorio.save(producto);
-        } else{
+        } else {
             throw new ErrorServicio("El producto solicitado no existe");
         }
     }
-    
+
     @Transactional
-    public void bajaProducto(String id) throws ErrorServicio{
+    public void bajaProducto(String id) throws ErrorServicio {
         Optional<Producto> respuestaProducto = productoRepositorio.findById(id);
-        if(respuestaProducto.isPresent()){
+        if (respuestaProducto.isPresent()) {
             Producto producto = respuestaProducto.get();
-            
+
             producto.setAlta(Boolean.FALSE);
-            
+
             productoRepositorio.save(producto);
-        } else{
+        } else {
             throw new ErrorServicio("EL producto solicitado no existe");
         }
     }
-    
+
     @Transactional
-    public void altaProducto(String id) throws ErrorServicio{
+    public void altaProducto(String id) throws ErrorServicio {
         Optional<Producto> respuestaProducto = productoRepositorio.findById(id);
-        if(respuestaProducto.isPresent()){
+        if (respuestaProducto.isPresent()) {
             Producto producto = respuestaProducto.get();
-            
+
             producto.setAlta(Boolean.TRUE);
-            
+
             productoRepositorio.save(producto);
-        } else{
+        } else {
             throw new ErrorServicio("EL producto solicitado no existe");
         }
     }
