a6456206277cde40663b213f3abd2e880b41ecc2
Ignacio Barolo <baroloignacio@gmail.com>
2022-01-24 12:00:28 -0300

    Agregando Login, Seguridad y th Fragments

--- VentasSpring/src/main/java/VentasSpring/VentasSpringApplication.java
+++ VentasSpring/src/main/java/VentasSpring/VentasSpringApplication.java
@@ -1,13 +1,24 @@
 package VentasSpring;
 
+import VentasSpring.Servicios.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class VentasSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VentasSpringApplication.class, args);
-	}
+    @Autowired
+    private UsuarioServicio usuarioServicio;
 
+    public static void main(String[] args) {
+        SpringApplication.run(VentasSpringApplication.class, args);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usuarioServicio).passwordEncoder(new BCryptPasswordEncoder());
+    }
 }
