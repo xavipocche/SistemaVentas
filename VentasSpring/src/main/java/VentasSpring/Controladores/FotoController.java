2b4a246bcc7dce37a72dc12b8cea669f79a34afb
Xavier Pocchettino <xavierpocchettino@gmail.com>
2022-07-05 21:41:38 -0300

    Quitado de entidades
    Una vez verificado el funcionamiento del pedido se agregaran las entidades quitadas

--- VentasSpring/src/main/java/VentasSpring/Controladores/FotoController.java
+++ VentasSpring/src/main/java/VentasSpring/Controladores/FotoController.java
@@ -16,7 +16,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
